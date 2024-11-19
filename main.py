458b6a5ccb863b2b09d818c0c7023519c68bf26d
Engelo <102564245+RassikDemonSlayer@users.noreply.github.com>
2024-11-19 20:47:31 +0600

    Add files via upload
    Initial commit
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md

---
+++ main.py
@@ -0,0 +1,41 @@
+from time import sleep
+import subprocess
+import pyperclip
+import pyautogui
+
+from set_up import osu_path, map_name_correct, screen_width_center, screen_height_center, hit_objects, template
+from funcs import press_ctrl_v, press_enter, monitor_screen_with_raw_template, play_map
+
+def start_map():
+    """Выбор и заход на карту"""
+    sleep(8)
+    pyautogui.moveTo(screen_width_center, screen_height_center, duration=0.1)
+    sleep(0.3)
+    pyautogui.click()
+    sleep(0.5)
+    pyautogui.moveTo(1230, 320)
+    sleep(0.3)
+    pyautogui.click()
+    sleep(0.5)
+    pyautogui.moveTo(1250, 395)
+    sleep(0.3)
+    pyautogui.click()
+    sleep(0.5)
+    press_ctrl_v()
+    sleep(1)
+    press_enter()
+    match_time = monitor_screen_with_raw_template(template, region=None, template_size=(60, 60))
+    play_map(match_time, hit_objects)
+    
+    
+def main():
+    pyperclip.copy(map_name_correct) #Копируем в буффер название трека чтобы попасть только на одну карту
+    osu_process = subprocess.Popen(osu_path)
+
+    start_map()
+    
+    osu_process.wait()
+    
+if __name__ == "__main__":
+    pass
+    main()
---
+++ set_up.py
@@ -0,0 +1,83 @@
+import re
+import pyautogui
+from funcs import osu_to_screen
+
+#========================= Настройки и переменные ====================================================================
+screen_width, screen_height = pyautogui.size() #Разрешение экрана
+screen_width_center, screen_height_center = (int(screen_width/2), int(screen_height/2)) #Координаты центра экрана
+
+start_config_file = "start_config.txt" #файл с конфигом
+
+region = {"top": 60, "left": 1700, "width": 77, "height": 60} #Область поиска совпадения на экране
+template = "objects\\end_timer.raw"
+#=====================================================================================================================
+
+
+
+#============================ Парсинг конфига ========================================================================
+config = []
+with open(start_config_file, 'r', encoding='utf-8') as file:
+    """Получаем информацию из текстового документа о путях к самой осу, карте и т.д."""
+    config_list = list(file)
+    for line in config_list:
+        line = line.split(": ")
+        config.append(line[1])
+
+def correct_map_name(map_name):
+    """Получение корректного имени карты"""
+    pattern = r"\([^)]*\)|\.osu" #Паттерн для удаления расширения и содержимого круглых скобок
+    cleaned_string = re.sub(pattern, "", map_name).strip() # Убираем ненужные части
+    
+    square_brackets_pattern = r"\[([^\]]+)\]" # Извлечение текста из квадратных скобок
+    square_content = re.search(square_brackets_pattern, cleaned_string)
+    
+    if square_content: #Удаляем квадратные скобки и добавляем содержимое к оставшемуся тексту
+        final_string = re.sub(r"\[.*?\]", "", cleaned_string).strip() + " " + square_content.group(1)
+    else:
+        final_string = cleaned_string
+    
+    return final_string
+
+
+osu_path = config[0].strip() #Путь до osu!.exe
+map_path = config[1].strip() #Папка с картой
+map_name = config[2].strip() #Название карты
+map_name_correct = correct_map_name(map_name) #Имя файла без лишних скобок для вставки
+map_full_path = map_path + "\\" + map_name #Полный путь до карты
+del config
+#=======================================================================================================================
+
+
+
+#========================== Парсинг Osu! файла =========================================================================
+
+hit_objects = []
+with open(map_full_path, 'r', encoding='utf-8') as file:
+    osu_data = file.read()
+
+# Извлечение строк с hit objects
+hit_objects_section = osu_data.split("[HitObjects]")[1].split("[")[0].strip()
+
+for line in hit_objects_section.split("\n"):
+    if line:  # Пропускаем пустые строки
+        # Разделяем строку на части и извлекаем x, y, hit_time
+        parts = line.split(",")
+        x = int(parts[0])
+        y = int(parts[1])
+        hit_time = int(parts[2])
+        hit_objects.append([x, y, hit_time])
+for hit_obj in hit_objects: 
+    hit_obj[2] /= 1000
+
+#=======================================================================================================================
+def prepare_hit_objects(hit_objects):
+    """Подготовка данных: конвертация osu-координат в экранные."""
+    prepared_objects = []
+    for hit_obj in hit_objects:
+        x, y, time = hit_obj
+        screen_x, screen_y = osu_to_screen(x, y)  # Конвертируем координаты
+        prepared_objects.append((screen_x, screen_y, time))
+    return prepared_objects
+
+hit_objects = prepare_hit_objects(hit_objects)
+
